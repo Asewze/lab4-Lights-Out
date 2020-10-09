4c9c8cab20a8dc89a473a02ffb562fcf56b6ff31
Asewze <adrielreyes520@gmail.com>
2020-10-09 13:40:39 -0700

    Put in Board in

--- lab4.py
+++ lab4.py
@@ -1,6 +1,8 @@
 # Unicode for empty box, u"\u25A1"
 # Unicode for black box, u"\u25A0"
 # can double check if Unicodes are true
+import random
+
 board = [
     [X, X, X, X, X],
     [X, X, X, X, X],
@@ -12,11 +14,18 @@ board = [
 
 def main():
 
+    for board in square:
+        print(board)
+
+
     def index_to_row(row):
         row // 5
 
     def index_to_col(col):
         col // 5
 
-    index_to_col(input('Please choose a column number (0 , -4): '))
-    index_to_row(input('Please choose a row number (0, -4): '))
+    index_to_col(input('Please choose a column number (0-4): '))
+    index_to_row(input('Please choose a row number (0-4): '))
+
+
+main()
